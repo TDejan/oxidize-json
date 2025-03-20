1b30e8fa50e343abef41f63a5bbff88d5bd544c0
TDejan <dejanbltamamovic@gmail.com>
2025-03-20 16:12:10 +0100

    add simple module with submodule

--- oxidize-json/src/main.rs
+++ oxidize-json/src/main.rs
@@ -1,8 +1,12 @@
-mod generator;
+// The binary crate's entry point (contains `main`)
+
+mod core;
+
+// A path is a way of naming items (like functions, structs, modules) in Rust. Paths can be absolute (from the root of the crate) or relative (from the current module).
+// use core::generator; brings the generator module into scope.
+use core::generator as gen; // A module inside the crate for organizing code
 
 fn main() {
-    println!("Hello, world!");
-
-    generator::parse_json();
-    generator::generate_schema();
+    gen::parse_json();
+    gen::generate_schema();
 }
